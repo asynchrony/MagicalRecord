1d33a73230e73558ca9a7fb672eb26f04fc6cf28
Nate Minshew <nate.minshew@asolutions.com>
2012-12-12 10:17:40 -0600

    Added framework target.

---
+++ Project Files/Magical Record Framework/Magical_Record_Framework.h
@@ -0,0 +1,13 @@
+//
+//  Magical_Record_Framework.h
+//  Magical Record Framework
+//
+//  Created by Nate Minshew on 12/12/12.
+//  Copyright (c) 2012 Magical Panda Software LLC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Magical_Record_Framework : NSObject
+
+@end
---
+++ Project Files/Magical Record Framework/Magical_Record_Framework.m
@@ -0,0 +1,13 @@
+//
+//  Magical_Record_Framework.m
+//  Magical Record Framework
+//
+//  Created by Nate Minshew on 12/12/12.
+//  Copyright (c) 2012 Magical Panda Software LLC. All rights reserved.
+//
+
+#import "Magical_Record_Framework.h"
+
+@implementation Magical_Record_Framework
+
+@end
